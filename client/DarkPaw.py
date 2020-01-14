daf5e4d5ac12722a7b58556888c70a276c7e724d
pi <89375638+sect16@users.noreply.github.com>
2020-01-14 08:41:50 +0000

    Added universal ultrasonic function. Able to Ultrasonic enable/disable ultrasonic function individually. Modified status_data to recognize exact text rather than 'in'.

--- client/DarkPaw.py
+++ client/DarkPaw.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python
-# -*- coding: UTF-8 -*-
-# File name   : client.py
-# Description : Re-write darkpaw client
-# E-mail	  : sect16@gmail.com
-# Author	  : Chin Pin Hon
-# Date		: 02/12/2019
+# Description : DarkPaw client
+# E-mail      : sect16@gmail.com
+# Author      : Chin Pin Hon
+# Date        : 14.01.2020
 # 
 
 import gui
-
 
 if __name__ == '__main__':
     gui.loop()
--- client/key_bind.py
+++ client/key_bind.py
@@ -1,3 +1,13 @@
+# E-mail      : sect16@gmail.com
+# Author      : Chin Pin Hon
+# Date        : 14.01.2020
+
+"""
+This file contains robot specific key binding config.
+
+It is meant to be executed in line and not imported.
+"""
+
 root.bind('<KeyPress-w>', call_forward)
 root.bind('<KeyPress-s>', call_back)
 root.bind('<KeyPress-a>', call_left)
@@ -13,15 +23,17 @@ root.bind('<KeyRelease-s>', call_stop)
 root.bind('<KeyRelease-a>', call_turn_stop)
 root.bind('<KeyRelease-d>', call_turn_stop)
 
-root.bind('<KeyPress-h>', call_head_home)
-root.bind('<KeyPress-i>', call_head_up)
-root.bind('<KeyPress-k>', call_head_down)
+root.bind('<KeyPress-h>', lambda _: functions.send('headhome'))
+root.bind('<KeyPress-i>', lambda _: functions.send('headup'))
+root.bind('<KeyPress-k>', lambda _: functions.send('headdown'))
 root.bind('<KeyPress-x>', call_find_color)
 root.bind('<KeyPress-c>', call_watchdog)
-root.bind('<KeyPress-b>', call_stream_audio)
-root.bind('<KeyPress-j>', call_head_left)
-root.bind('<KeyPress-l>', call_head_right)
-root.bind('<KeyPress-u>', call_head_low)
-root.bind('<KeyPress-o>', call_head_high)
+
+root.bind('<KeyPress-b>', lambda _: functions.send('stream_audio'))
+root.bind('<KeyPress-j>', lambda _: functions.send('headleft'))
+root.bind('<KeyPress-l>', lambda _: functions.send('headright'))
+root.bind('<KeyPress-u>', lambda _: functions.send('low'))
+root.bind('<KeyPress-o>', lambda _: functions.send('high'))
+
 root.bind('<KeyPress-z>', call_steady)
 root.bind('<KeyPress-v>', call_smooth)
